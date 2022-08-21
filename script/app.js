3b086f6f401dc20f3e55f60334967c8a8c71d137
mamunur1296 <programer597@gmail.com>
2022-08-22 05:03:20 +0600

    player expences calculation dan

--- script/app.js
+++ script/app.js
@@ -5,25 +5,32 @@ function playerToArray(plArray) {
         const Name = plArray[i].playerName;
         const serialNo = i + 1;
         const tr = document.createElement("tr");
+        // tr.className = "hi"
         tr.innerHTML = `
-         <td class="col-4">${serialNo}</td>
+         <td class="col-4 hi">${serialNo}</td>
          <td class="col-8">${Name}</td>
         `;
         tableBody.appendChild(tr);
-
     }
-    console.log(tableBody.childNodes.length);
 }
 const playerArray = [];
 function selectBtn(select) {
+    select.disabled = true;
     const playerName = select.parentNode.childNodes[1].innerHTML;
     const playerObj = {
         playerName: playerName,
     }
     playerArray.push(playerObj);
+    document.getElementById("plair-list").innerText = playerArray.length;
+    if (playerArray.length >= 6) {
+        return;
+    }
+    console.log(playerArray.length);
     playerToArray(playerArray);
 }
-
-
-
-
+document.getElementById("calculate-btn").addEventListener("click", function () {
+    const inputfileOne = inputFildById("input-cost");
+    const playerParOrder = elementFildById("plair-list");
+    const totalPlayerExp = inputfileOne * playerParOrder;
+    gatRejult("player-exp", totalPlayerExp);
+})
--- script/utilities.js
+++ script/utilities.js
@@ -1,1 +1,16 @@
-console.log("hallo utitlities");
+function inputFildById(input) {
+    const inputfild = document.getElementById(input);
+    const inputfildString = inputfild.value;
+    const inputFildNam = parseFloat(inputfildString);
+    return inputFildNam;
+}
+function elementFildById(element) {
+    const elementFild = document.getElementById(element);
+    const elementFildString = elementFild.innerText;
+    const elementFildNumber = parseFloat(elementFildString);
+    return elementFildNumber;
+}
+function gatRejult(elementId, elementValue) {
+    const rejult = document.getElementById(elementId);
+    rejult.innerText = elementValue;
+}
